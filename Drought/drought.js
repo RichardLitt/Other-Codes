5866c43554ff038e93816dd9f7b992b71e290ea6
Richard Littauer <richard.littauer@gmail.com>
2013-01-31 17:42:41 +0100

    Added comments, readme.

--- Drought/drought.js
+++ Drought/drought.js
@@ -1,12 +1,17 @@
+// Get rid of the sections you don't like
 function clear_page() {
+    // I'm personally not a fan of the blue Status bar
     document.getElementsByClassName('_li')[0] = document.getElementsByClassName('_li')[0].removeChild(document.getElementById('pagelet_bluebar'));
+    // Or the rest of the page, to be honest
     document.getElementsByClassName('_li')[0] = document.getElementsByClassName('_li')[0].removeChild(document.getElementById('globalContainer'));
+    // The sidebar might be nice, if you want to know which friends are on.
     document.getElementsByClassName('_li')[0] = document.getElementsByClassName('_li')[0].removeChild(document.getElementById('pagelet_sidebar'));
 };
 
 function add_image() {
     var mydiv = document.getElementsByClassName('_li');
     var newcontent = document.createElement('div');
+    // Edit the src to point to a wallpaper you, you know, like.
     newcontent.innerHTML = '<img style="-webkit-user-select: none; cursor: -webkit-zoom-in;" src="http://i.imgur.com/wvunc.jpg" width="1280">';
 
     while (newcontent.firstChild) {
@@ -14,8 +19,7 @@ function add_image() {
     }
 };
 
-//chrome.browserAction.onClicked.addListener(clear_page);
-//chrome.browserAction.onClicked.addListener(add_image);
+// And we're done here, folks.
 clear_page();
 add_image();
 
---
+++ Drought/readme.md
@@ -0,0 +1,8 @@
+This is a chrome extension to get rid of the distracting bits of
+Facebook, while still allowing you to talk to the few friends you were
+already talking to. Optionally, you can include the list of friends
+online - personally, I'd rather not. 
+
+To install, download drought.zip. Unpack it. Go to chrome://extensions
+in the browser, and click "Load unpacked extensions...". Enable or
+disable as desired. Go to Facebook. Press refresh. Enjoy. 
