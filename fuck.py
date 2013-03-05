87ee2166765934ab0679dea52fad8d72f507bbbc
Richard Littauer <richard.littauer@gmail.com>
2013-03-05 15:24:43 +0100

    Added some more phrases and a library.

--- fuck.py
+++ fuck.py
@@ -1,5 +1,20 @@
 #!/Library/Frameworks/Python.framework/Versions/Current/bin/python
 #-*- coding: utf-8 -*-
 
+"""
+This is just a script that makes a response every time I yell at my computer.
+Make an alias and change the responses, if you like.
+
+All the licenses are on this beast. 
+"""
+
+import random
+
+library = [
+  'Oswald was a fag.',
+  'Do it for the boat people.',
+  'brain dog is dead'
+        ]
+
 if __name__ == '__main__':
-    print "Oswald was a fag."
+	print library[random.randrange(0,len(library))]
